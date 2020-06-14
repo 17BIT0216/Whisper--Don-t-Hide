c082620b1453ab5af11e7401bd95c2899a1309b3
Yarun <arungiri@Aruns-MacBook-Air.local>
2020-06-14 08:12:09 +0530

    So we used bcrypt in the preeviously now we replace with passport, because if automatically add hashing and salts and also it provides the cookies. SO we used passport with express session which maintains the session and all passport-local-mangoose to store the credentials to the data base. Mainly the functions provided by the the passport is .register to add the credentials to the DB then .authenticate to authenticate the current user and provide a cookie so he does not has to login again and again till he has the cookie. Then req.Authenticate to authenticate the incoming requeest and req.login() and req.logout() to logout

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const md5=require("md5");
+// const md5=require("md5");
+// const bcrypt=require("bcrypt");
+// const saltRounds=10;
+const session = require('express-session');
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
+
 
 const app=express();
 
@@ -12,20 +18,37 @@ app.use(express.static("public"));
 app.set('view engine','ejs'); //where ejs files will be present
 app.use(bodyParser.urlencoded({extended:true}));
 
+//this must me admitedly be here
+app.use(session({
+  secret: 'I Love my Country',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/AuthenticDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
   name:String,
   password:String,
 })
 
-
+userSchema.plugin(passportLocalMongoose);
 
 //userSchema.plugin(encrypt,{secret:process.env.YESECRET, excludeFromEncryption: ['name']});
 //so when ever the username is saved it is encrypted first
 //and when we try to find the password it is automatically decrypted
 
 const User=new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy()); //this refers to the strategy we used
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser()); //serializes and stores the data
+passport.deserializeUser(User.deserializeUser());//deserializes and displays the data
 
 app.get("/",function(req,res)
 {
@@ -42,46 +65,67 @@ app.get("/register",function(req,res)
   res.render("register");
 });
 
+app.get("/secrets",function(req,res)
+{
+  //now here we authenticate our user with his session
+  if(req.isAuthenticated())   //if the request is authentic
+  res.render("secrets");
+  else
+  res.redirect("/login");
+})
+
 
 app.post("/register",function(req,res)
 {
-  const user=new User(
-    {name:req.body.username,
-      password:md5(req.body.password),
-    });
-    //created a new user
-    user.save(function(err)
+  //here the variable names are fixed has tobe username and password
+User.register({username:req.body.username},req.body.password,function(err,result)
+{
+  if(err)
   {
-    if(!err)
-    {
-      res.render("secrets");
-    }
-  })
+    console.log(result);
+    console.log(err);
+  res.redirect("/register");
+}
+  else{
+  passport.authenticate("local")(req,res,function(){
+  res.redirect("/secrets"); //now the case here is the user can go directely to the secrets
 });
+}
+})
+});
+
+app.get("/logout",function(req,res)
+{
+  req.logout();
+  res.redirect('/register'); 
+})
 
 
 app.post("/login",function(req,res)
 {
-  const username=req.body.username;
-  const password=md5(req.body.password); //here we hash our password again
-  User.findOne({name:username},function(err,foundArticle)
-{
-if(err)
-{
-  console.log(err);
-}
-else
-{
-  if(foundArticle)
+const newUser= new User(
   {
-    if(foundArticle.password===password)
-    res.render("secrets");
-    else
-    console.log("password entered was incorrect");
+    name:req.body.username,
+    password:req.body.password,
   }
-}
-})
+);
 
+req.login(newUser,function(err)
+{
+  if(err)
+  {
+  res.redirect("/login");
+}
+  else
+  {
+    //if no errors then we authenticate the user and give him a cookie so that he may be rememberred
+    passport.authenticate("local")(req,res,function(){
+    res.redirect("/secrets");
+  });
+}
+
+
+});
 });
 
 
